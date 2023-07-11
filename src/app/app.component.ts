7557c04a3cb5493d543bd833ab1b4f8b2cbef1b9
Jayashri24 <sjayashri7@gmail.com>
2023-07-11 15:23:03 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  id=101;
+  title = 'Hiii friends I am ';
+  name='Jayu';
+}
